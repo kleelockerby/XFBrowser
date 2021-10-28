50620986d131e8754ce0dae716e235d7abdae301
Kevin Lockerby <klockerby@onestreamsoftware.com>
2021-10-28 11:20:37 -0400

    Add Fluent

--- XFBrowser5/Views/LogonForm.cs
+++ XFBrowser5/Views/LogonForm.cs
@@ -19,18 +19,31 @@ namespace XFBrowser5
             InitializeComponent();
             if (!mvvmContext.IsDesignMode)
             {
-                InitializeBindings();
+                //InitializeBindings();
+                InitializeBindingsFluent();
             }
         }
 
         public void InitializeBindings()
         {
             LogonFormViewModel viewModel = mvvmContext.GetViewModel<LogonFormViewModel>();
+            
             txtUserName.DataBindings.Add("EditValue", viewModel, "UserName");
             txtPassword.DataBindings.Add("EditValue", viewModel, "Password");
             lblResult.DataBindings.Add("Text", viewModel, "Result");
+            
             btnLogon.BindCommand(new DelegateCommand(() => { viewModel.LogonUser(); }));
-            //btnLogon.BindCommand(new DelegateCommand(() => { viewModel.DoSomething(); }));
+        }
+
+        public void InitializeBindingsFluent()
+        {
+            var fluent = mvvmContext.OfType<LogonFormViewModel>();
+            
+            fluent.SetBinding(txtUserName, ed => ed.EditValue, x => x.UserName);
+            fluent.SetBinding(txtPassword, ed => ed.EditValue, x => x.Password);
+            fluent.SetBinding(lblResult, l => l.Text, x => x.Result);
+
+            fluent.BindCommand(btnLogon, x => x.LogonUser);
         }
     }
 }
